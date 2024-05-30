8b32ba5e4ecf3ee30a1e9f84b7aae156b61525ed
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-05-30 04:29:11 +0300

    create item model

---
+++ lib/models/item.dart
@@ -0,0 +1,14 @@
+class Item {
+  bool isExpanded ;
+   String header;
+   String body;
+   String selection;
+
+  Item({
+    this.isExpanded = false,
+    required this.header,
+    required this.body,
+    required this.selection
+  });
+
+}
